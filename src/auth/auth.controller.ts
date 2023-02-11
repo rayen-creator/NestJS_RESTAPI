e46a4e55a96051b7a3db5e97c6d178d3eff9ef21
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-11 22:28:48 +0100

    ✅ update endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
+import { AuthDto } from './dto/auth.dto';
 import { Controller, Post } from "@nestjs/common";
+import { Body } from "@nestjs/common/decorators";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -6,12 +8,13 @@ export class AuthController{
 constructor(private authService: AuthService){}
 
 @Post('signup')
-signup(){
-return this.authService.signup();
+signup(@Body() authDto: AuthDto){
+    
+return this.authService.signup(authDto);
 }
 
 @Post('signin')
-signin(){
-    return this.authService.signin();
+signin(@Body() authDto: AuthDto){
+    return this.authService.signin(authDto);
 }
 }
