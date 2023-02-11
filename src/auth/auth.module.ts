61a2ecf480380b9acda024dfbd6a4d897eebb9ae
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-11 22:28:27 +0100

    ✅ create signin & signup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
