90fe01be1ed59aa83b652d160f33d8cc492202a2
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-16 23:09:30 +0100

    ✨ added jwt strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,15 +1,29 @@
+import { PrismaService } from './../../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config/dist';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private config:ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy,'jwt') {
+    constructor( config:ConfigService,private prismaService:PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('JWT_SECRET'),
         });
 
+    }
+
+    async validate(payload:{sub :number,email:string}){
+        // console.log({
+        //     payload,
+        // });
+        const user=await this.prismaService.user.findUnique({
+            where:{
+                id:payload.sub,
+            },
+        });
+        delete user.hash;
+        return user;
     }
 }
