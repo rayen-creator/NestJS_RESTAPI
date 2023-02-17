0e907e2f92674421fd63742714c7923c01d9699d
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-17 13:10:38 +0100

    ✅ update auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,20 @@
 import { AuthDto } from './dto/auth.dto';
-import { Controller, Post } from "@nestjs/common";
-import { Body } from "@nestjs/common/decorators";
+import { Controller, HttpStatus, Post } from "@nestjs/common";
+import { Body, HttpCode } from "@nestjs/common/decorators";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController{
 constructor(private authService: AuthService){}
 
+// @HttpCode(HttpStatus.OK)
 @Post('signup')
 signup(@Body() authDto: AuthDto){
     
 return this.authService.signup(authDto);
 }
-
+ 
+@HttpCode(HttpStatus.OK)
 @Post('signin')
 signin(@Body() authDto: AuthDto){
     return this.authService.signin(authDto);
