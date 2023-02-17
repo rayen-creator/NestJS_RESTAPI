ecbf3d96b2fa73d942d08dd9d361a1833b1afb9e
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-17 13:11:03 +0100

    🚑 few change to get user object from token

--- src/auth/decorator/get-userdecorator.ts
+++ src/auth/decorator/get-userdecorator.ts
@@ -1,8 +1,21 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const GetUser = createParamDecorator(
-    (data:unknown ,ctx:ExecutionContext)=>{
-        const req : Express.Request= ctx.switchToHttp().getRequest();
-        return req.user;
-    } 
-)
+import {
+    createParamDecorator,
+    ExecutionContext,
+  } from '@nestjs/common';
+  
+  //get user object from token
+  export const GetUser = createParamDecorator(
+    (
+      data: string | undefined,
+      ctx: ExecutionContext,
+    ) => {
+      const request: Express.Request = ctx
+        .switchToHttp()
+        .getRequest();
+      if (data) {
+        return request.user[data];
+      }
+      return request.user;
+    },
+  );
+  
